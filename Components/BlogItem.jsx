daaba854f5ed48f153923734b614a6582d987098
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2024-08-22 12:33:39 +0600

    Blog Page ready

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -1,18 +1,21 @@
 import { assets, blog_data } from '@/Assets/assets'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
-const BlogItem = ({title,description,category,image}) => {
+const BlogItem = ({title,description,category,image,id}) => {
   return (
       <div className='max-w-[300px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]'>
-          <Image src={image} alt='' width={400} height={400} className='border-b border-black' />
+          <Link href={`/blog/${id}`}>
+              <Image src={image} alt='' width={400} height={400} className='border-b border-black' />
+              </Link>
           <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-sm'>{category}</p>
           <div className="p-5">
               <h5 className='mb-2 text-lg font-medium tracking-tighter text-gray-900'>{title}</h5>
               <p className='mb-3 text-sm tracking-tighter text-gray-700'>{description}</p>
-              <div className='inline-flex items-center py-2 font-semibold text-center'>
+              <Link href={`/blog/${id}`} className='inline-flex items-center py-2 font-semibold text-center'>
               Read more <Image width={12} className='ml-2' src={assets.arrow} alt=''  /> 
-              </div>
+              </Link>
           </div>
       </div>
   )
