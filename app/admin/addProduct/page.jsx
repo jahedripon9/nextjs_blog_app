668c02756c1496094ab0e190742a1ac4d8d1a798
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2024-08-27 10:01:22 +0600

    page to Page Rename

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import { useState } from 'react'
 import { toast } from 'react-toastify'
 
-const page = () => {
+const Page = () => {
 
     const [image, setImage] = useState(false);
     const [data, setData] = useState({
@@ -76,4 +76,4 @@ const page = () => {
     )
 }
 
-export default page
+export default Page
