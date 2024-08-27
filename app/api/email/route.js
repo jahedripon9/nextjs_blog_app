711fa234e2556878a77092b82a10d8fa27f470e3
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2024-08-27 14:07:02 +0600

    Email Subscription Show list on MongoDB Database to Admin panel

--- app/api/email/route.js
+++ app/api/email/route.js
@@ -15,3 +15,14 @@ export async function POST(request) {
     await EmailModel.create(emailData);
     return NextResponse.json({ success: true, msg: "Email Subscribed" })
 }
+
+export async function GET(request) {
+    const emails = await EmailModel.find({});
+    return NextResponse.json({ emails })
+}
+
+export async function DELETE(request) {
+    const id = await request.nextUrl.searchParams.get("id");
+    await EmailModel.findByIdAndDelete(id);
+    return NextResponse.json({ success: true, msg: "Email Deleted" })
+}
--- app/blog/[id]/page.jsx
+++ app/blog/[id]/page.jsx
@@ -40,19 +40,9 @@ const page = ({ params }) => {
         </div>
         <div className='mx-5 max-w-[800px] md:mx-auto mt-[-100px] mb-10'>
             <Image className='border-4 border-white' src={data.image} alt='' width={1280} height={720} />
-            <h1 className='my-8 text-[26px] font-semibold'>Introduction:</h1>
-            <p className=''>{data.description}</p>
-            <h3 className='my-5 text-[18px] font-semibold'>Step 1: Self-Reflection and Goal Setting</h3>
-            <p className='my-3 '>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores aperiam ab, quam ducimus adipisci ex atque voluptatibus corrupti molestiae ullam autem delectus voluptates magnam, quidem architecto tempore provident perspiciatis esse!</p>
-            <p className='my-3 '>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores aperiam ab, quam ducimus adipisci ex atque voluptatibus corrupti molestiae ullam autem delectus voluptates magnam, quidem architecto tempore provident perspiciatis esse!</p>
-            <h3 className='my-5 text-[18px] font-semibold'>Step 2: Self-Reflection and Goal Setting</h3>
-            <p className='my-3 '>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores aperiam ab, quam ducimus adipisci ex atque voluptatibus corrupti molestiae ullam autem delectus voluptates magnam, quidem architecto tempore provident perspiciatis esse!</p>
-            <p className='my-3 '>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores aperiam ab, quam ducimus adipisci ex atque voluptatibus corrupti molestiae ullam autem delectus voluptates magnam, quidem architecto tempore provident perspiciatis esse!</p>
-            <h3 className='my-5 text-[18px] font-semibold'>Step 3: Self-Reflection and Goal Setting</h3>
-            <p className='my-3 '>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores aperiam ab, quam ducimus adipisci ex atque voluptatibus corrupti molestiae ullam autem delectus voluptates magnam, quidem architecto tempore provident perspiciatis esse!</p>
-            <p className='my-3 '>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores aperiam ab, quam ducimus adipisci ex atque voluptatibus corrupti molestiae ullam autem delectus voluptates magnam, quidem architecto tempore provident perspiciatis esse!</p>
-            <h3 className='my-5 text-[18px] font-semibold'>Conclusion:</h3>
-            <p className='my-3 '>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore corrupti eum fuga iusto quam, optio laboriosam vitae quidem iure nihil.</p>
+            
+            <div className='blog-content' dangerouslySetInnerHTML={{ __html: data.description }}></div>
+        
             <div className='my-24'>
                 <p className='text-black font-semibold my-4 '>Share this article on Social Media</p>
                 <div className='flex'>
